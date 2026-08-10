c6b17033d7f13488d87d14fdb7a360b6efed217d
Mayur <cloudgenz.skillsquirrel@gmail.com>
2026-08-10 11:55:12 +0530

    increase body parser size limit to 50mb

--- backend/index.js
+++ backend/index.js
@@ -39,8 +39,8 @@ app.use(cors({
   credentials: true
 }));
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true })); 
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ extended: true, limit: '50mb' })); 
 
 const uploadPath = path.join(process.cwd(), '../uploads');
 app.use('/uploads', express.static(uploadPath));
